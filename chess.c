ad4c1340681c29d396103a188b09fb3f081123f4
miten <57693631+m1ten@users.noreply.github.com>
2023-10-21 14:56:14 -0400

    Update chess.c

--- chess.c
+++ chess.c
@@ -22,23 +22,31 @@ typedef struct {
 void setAlgebraic(Matrix *matrix, int toPrint);
 void pathfinder(Matrix *matrix, Point *point);
 
-int main() {
+int main()
+{
   // read in an 8x8 matrix from standard input
   // store the location as x and y coordinates
   Matrix matrix;
-  int x, y = 0;
+  int x = 0, y = 0;
   char value;
-  while (scanf("%c", &value) != EOF) {
+
+  while (scanf("%c", &value) != EOF)
+  {
     // if newline, increment y and reset x
-    if (value == '\n') {
+    if (value == '\n')
+    {
       x = 0;
       y++;
-    } else {
+    }
+    else
+    {
       // store the value in the matrix if within range
-      if (x < 8 && y < 8) {
-        matrix.points[x + y * 8].x = x;
-        matrix.points[x + y * 8].y = y;
-        matrix.points[x + y * 8].value = value;
+      if (x < 8 && y < 8)
+      {
+        int index = x + y * 8;
+        matrix.points[index].x = x;
+        matrix.points[index].y = y;
+        matrix.points[index].value = value;
       }
       x++;
     }
@@ -48,58 +56,40 @@ int main() {
   setAlgebraic(&matrix, 1);
 
   // find the queen(s) [q]
-  for (int i = 0; i < 64; i++) {
-    if (matrix.points[i].value == 'q') {
+  for (int i = 0; i < 64; i++)
+  {
+    if (matrix.points[i].value == 'q')
+    {
       printf("\nFound a queen at %s\n", matrix.points[i].algebraic);
 
       // find and print the queen's possible moves
       pathfinder(&matrix, &matrix.points[i]);
     }
   }
+
   return 0;
 }
 
-// print out the matrix
-void printMatrix(Matrix matrix) {
-  /* Format:
-    +-+-+-+-+-+-+-+-+
-  8 |R|N|B|Q|K|B|N|R|
-    +-+-+-+-+-+-+-+-+
-  7 |P|P|P|P|P|P|P|P|
-    +-+-+-+-+-+-+-+-+
-  6 | | | | | | | | |
-    +-+-+-+-+-+-+-+-+
-  5 | | | | | | | | |
-    +-+-+-+-+-+-+-+-+
-  4 | | | | | | | | |
-    +-+-+-+-+-+-+-+-+
-  3 | | | | | | | | |
-    +-+-+-+-+-+-+-+-+
-  2 |p|p|p|p|p|p|p|p|
-    +-+-+-+-+-+-+-+-+
-  1 |r|n|b|q|k|b|n|r|
-    +-+-+-+-+-+-+-+-+
-     a b c d e f g h
-  */
-
+void printMatrix(Matrix matrix)
+{
   // print the top border
   printf("Board: \n  +-+-+-+-+-+-+-+-+\n");
 
-  // print the matrix using the algebraic notation and value (a8 = top left, h1
-  // = bottom right)
-  for (int i = 0; i < 64; i++) {
-    // print the left border
-    if (i % 8 == 0) {
-      printf("%d |", 8 - i / 8);
+  // print the matrix using the algebraic notation and value (a8 = top left, h1 = bottom right)
+  for (int i = 0; i < 64; i++)
+  {
+    // print the left border, value, and right border in a single call to printf
+    if (i % 8 == 0)
+    {
+      printf("%d |%c|", 8 - i / 8, matrix.points[i].value);
     }
-
-    // print the value
-    printf("%c|", matrix.points[i].value);
-
-    // print the right border
-    if (i % 8 == 7) {
-      printf("\n");
-      printf("  +-+-+-+-+-+-+-+-+\n");
+    else if (i % 8 == 7)
+    {
+      printf("%c|\n  +-+-+-+-+-+-+-+-+\n", matrix.points[i].value);
+    }
+    else
+    {
+      printf("%c|", matrix.points[i].value);
     }
   }
 
@@ -112,16 +102,20 @@ void setAlgebraic(Matrix *matrix, int toPrint)
   // set the algebraic notation for each point
   // based on the x and y coordinates
   // a8 = top left, h1 = bottom right
-  char col_values[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
 
   for (int i = 0; i < 64; i++)
   {
     int x = matrix->points[i].x;
     int y = matrix->points[i].y;
 
+    if (x < 0 || x > 7 || y < 0 || y > 7)
+    {
+      continue;
+    }
+
     // set the algebraic notation for each point
-    matrix->points[i].algebraic[0] = col_values[x];
-    matrix->points[i].algebraic[1] = 8 - y + '0';
+    matrix->points[i].algebraic[0] = 'a' + x;
+    matrix->points[i].algebraic[1] = '8' - y;
     matrix->points[i].algebraic[2] = '\0';
   }
 
